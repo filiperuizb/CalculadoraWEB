2b58d56952d45dd8ce667ece6a93c4a32668a39a
filiperuizb <filiperuizboligon9@gmail.com>
2025-03-23 18:06:40 -0300

    Versão final 3.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,57 @@
 const display = document.getElementById("display");
+let n1 = "", n2 = "", operator = "";
+let clear = false;
 
 function appendToDisplay(input) {
-    display.value += input;
+    if (["+", "-", "*", "/"].includes(input)) {
+        if (n1 === "") {
+            n1 = display.value; 
+        }
+        
+        if (operator === "") { 
+            operator = input;
+            clear = true;
+        }
+    } else {
+        if (clear) {
+            display.value = "";  
+            clear = false;
+        }
+        display.value += input; 
+    }
 }
 
 function clearDisplay() {
     display.value = "";
+    n1 = "";
+    n2 = "";
+    operator = "";
 }
 
 function calculate() {
-    display.value = eval(display.value);
+    if (n1 !== "" && operator !== "" && display.value !== "") {
+        n2 = display.value; 
+        let result;
+
+        switch (operator) {
+            case '+':
+                result = parseFloat(n1) + parseFloat(n2);
+                break;
+            case '-':
+                result = parseFloat(n1) - parseFloat(n2);
+                break;
+            case '*':
+                result = parseFloat(n1) * parseFloat(n2);
+                break;
+            case '/':
+                result = parseFloat(n1) / parseFloat(n2);
+                break;
+        }
+
+        display.value = result;
+        n1 = result.toString();  
+        n2 = "";
+        operator = "";
+        clear = true;
+    }
 }
